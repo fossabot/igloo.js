c7889dab5669f3c878e03662024b81156731d7e7
Zanin Andrea <andrea@igloo.ooo>
2019-02-19 17:53:09 +0100

    async iterator over the environments

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const minimalGraphql = require("minimal-graphql")
 const gql = require("graphql-tag")
 
+function isNumeric(n) {
+    return typeof n !== "symbol" && !isNaN(parseFloat(n)) && isFinite(n)
+}
+
 function Igloo(bearer) {
     const client = minimalGraphql({
         uri: "https://igloo-production.herokuapp.com/graphql",
@@ -55,6 +59,94 @@ class User {
             })
             .then(res => res.data.user.name)
     }
+    get environments() {
+        const client = this.client
+
+        const handler = {
+            get: function(obj, prop) {
+                if (isNumeric(prop)) {
+                    return getEnvironment(prop, client)
+                } else if (prop === Symbol.asyncIterator) {
+                    return function() {
+                        let index = 0
+                        return {
+                            next: () =>
+                                getEnvironment(index, client).then(
+                                    environment => {
+                                        index++
+                                        return environment === undefined
+                                            ? { done: true }
+                                            : {
+                                                  value: environment,
+                                                  done: false,
+                                              }
+                                    }
+                                ),
+                        }
+                    }
+                } else {
+                    return obj[prop]
+                }
+            },
+        }
+
+        var lazyLoader = new Proxy({}, handler)
+        return lazyLoader
+    }
+}
+
+class Environment {
+    constructor(client, id) {
+        this.client = client
+        this.id = id
+    }
+
+    static fields() {
+        return ["id", "name"]
+    }
+
+    get name() {
+        return this.client
+            .query({
+                query: gql`
+                    {
+                        environment(id:"${this.id}") {
+                            id
+                            name
+                        }
+                    }
+                `,
+            })
+            .then(res => res.data.environment.name)
+    }
+}
+
+const getEnvironment = (index, client) => {
+    let promise = client
+        .query({
+            query: gql`
+                    {
+                        user {
+                            environments(offset:${index},limit:1){
+                                id
+                            }
+                        }
+                    }
+                `,
+        })
+        .then(res =>
+            res.data.user.environments.length === 0
+                ? undefined
+                : new Environment(client, res.data.user.environments[0].id)
+        )
+
+    Environment.fields().forEach(field => {
+        promise[field] = promise.then(environment =>
+            environment ? environment[field] : undefined
+        )
+    })
+
+    return promise
 }
 
 class Mutation {}
--- test.js
+++ test.js
@@ -2,9 +2,13 @@ const Igloo = require("./index")
 const bearer =
     "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJleHAiOjE1NTExMzI3MDUsInVzZXJJZCI6IjljMzg3MzliLTYzOTktNDQ1Ni04MWFkLTA2YTQyMDBkODA2NiIsImFjY2Vzc0xldmVsIjoiT1dORVIiLCJ0b2tlblR5cGUiOiJURU1QT1JBUlkifQ.jN_7939Qe2T2b0y5jWL6Km0YxLcn8iiEEucH8dwcCUM9QCBnMCOaR7IXji-Nx9-qzF9z5s5sPY5GbSfYHC2QUg"
 
-;(async () => {
+async function main() {
     const igloo = Igloo(bearer)
     const user = igloo.query.user
 
-    console.log(await user.name)
-})()
+    for await (let environment of user.environments) {
+        console.log(await environment.name)
+    }
+}
+
+main()
